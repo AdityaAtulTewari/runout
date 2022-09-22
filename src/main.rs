deca8de852ac863f6600531579c3b4839d5a7f1f
kgurazad <karangurazada@gmail.com>
2022-09-22 00:22:10 -0500

    rework code flow to place setrlimit and execvp closer together

--- src/main.rs
+++ src/main.rs
@@ -13,68 +13,85 @@ use libc::c_char;
 pub extern fn main(argc: i32, argv: *const *const c_char) -> i32
 {
   let start = Instant::now();
-  let time = must(sanitize(argc, argv));
-  let mut rlp = rlimit {rlim_cur: time, rlim_max: time+1};
+  let time = sanitize_input(argc, argv).unwrap();
+
+  let mut rlp = rlimit
+  {
+    rlim_cur: time,
+    rlim_max: time+1
+  };
   let rlpb: *const rlimit = &rlp;
+
   let s = start.elapsed().as_secs();
   rlp.rlim_cur = rlp.rlim_cur + s;
   rlp.rlim_max = rlp.rlim_max + s;
-  must(wrap_setrlimit(libc::RLIMIT_CPU, rlpb));
+
+  let function = unsafe{*argv.offset(2)};
+  let execvp_args = unsafe{argv.offset(2)};  
   //unsafe: pushing through argv to execvp
-  must(wrap_execvp(unsafe{*argv.offset(2)}, unsafe{argv.offset(2)}));
-  return -1;
+
+  wrap_setrlimit(libc::RLIMIT_CPU, rlpb).unwrap();
+  return wrap_execvp(function, execvp_args).unwrap();
 }
 
 #[inline]
 fn wrap_execvp(function: *const c_char, args: *const *const c_char) -> Result<(), &'static str>
 {
   //unsafe: call to execvp
+  let execvp_status;
   unsafe
   {
-    execvp(function, args);
+    execvp_status = execvp(function, args);
+  };
+
+  match execvp_status
+  {
+    0 => Ok(()),
+    _ => Err("Failed to set limits")
   }
-  return Err("Failed to exec");
 }
 
 fn wrap_setrlimit(resource: __rlimit_resource_t, rlp: *const rlimit) -> Result<(), &'static str>
 {
-  let err;
   //unsafe: call to setrlimit
+  let setrlimit_status;  
   unsafe
   {
-    err = setrlimit(resource, rlp);
-  }
-  if err != 0
+    setrlimit_status = setrlimit(resource, rlp);
+  };
+
+  match setrlimit_status
   {
-    return Err("Failed to set limits");
+    0 => Ok(()),
+    _ => Err("Failed to set limits")
   }
-  return Ok(());
 }
 
-fn sanitize(argc: i32, argv: *const *const c_char) -> Result<rlim_t, &'static str>
+fn sanitize_input(argc: i32, argv: *const *const c_char) -> Result<rlim_t, &'static str>
 {
-  if 2 > argc
+  if 2 != argc
   {
-    return Err("You must provide three arguments, runout [seconds] [COMMAND]");
+    return Err("You must provide two arguments: runout [seconds] [COMMAND]");
   }
+
   let num: *const c_char;
   //unsafe: parsing argument 1 in a clike fashion.
   unsafe
   {
     num = *argv.offset(1);
   }
+
   //unsafe: get zeroth char
   let mut curr: c_char = unsafe{*num};
   let mut i: isize = 0;
   let mut secs: rlim_t = 0;
-
   while curr != 0
   {
     match (curr as u8 as char).to_digit(10)
     {
-      Some(a) => {secs *= 10; secs += a as rlim_t;}
-      None => { return Err("The second argument must be a valid time_t number.");}
-    }
+      Some(a) => { secs *= 10; secs += a as rlim_t; }
+      None => { return Err("The second argument must be a valid time_t number."); }
+    };
     i+=1;
     //unsafe: get the ith char
     curr = unsafe{*num.offset(i)};
@@ -82,11 +99,3 @@ fn sanitize(argc: i32, argv: *const *const c_char) -> Result<rlim_t, &'static st
   return Ok(secs);
 }
 
-fn must<T, E: std::fmt::Display>(result: Result<T,E>) -> T
-{
-  match result
-  {
-    Ok(x) => {return x}
-    Err(x) => {panic!("{}", x)}
-  }
-}
